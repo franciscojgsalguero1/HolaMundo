680eca0fe302f71359bd6c9a8f06695991e33d2d
franciscojgsalguero1 <65239673+franciscojgsalguero1@users.noreply.github.com>
2020-12-24 18:24:51 +0100

    Imprime el Hola Mundo en PHP

---
+++ imprimirHolaMundo.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Imprimir Hola Mundo</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        $output = "";
+        $output .= 'hola mundo';
+        echo $output;
+        phpinfo();
+        ?>
+    </body>
+</html>
